9f4550546199a4e2068ef8406dd0624fc961634b
clevercoderjoy <clevercoderjoy.com>
2025-05-01 15:29:57 +0530

    built components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="bg-white shadow-md py-4">
+      <div className="max-w-screen-xl mx-auto px-2 flex items-center justify-between">
+        <div className="logo">
+          <h1 className="text-3xl font-bold text-blue-600 tracking-wide cursor-pointer">
+            <Link to="/">
+              cleverQuiz
+            </Link>
+          </h1>
+        </div>
+        <div className="navigation">
+          <nav>
+            <ul className="flex gap-6">
+              <li>
+                <Link
+                  to="/"
+                  className="text-blue-500 hover:text-white hover:bg-blue-500 border-2 border-blue-500 hover:border-blue-500 px-4 py-2 rounded-md font-bold transition-colors"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/results"
+                  className="text-blue-500 hover:text-white hover:bg-blue-500 border-2 border-blue-500 hover:border-blue-500 px-4 py-2 rounded-md font-bold transition-colors"
+                >
+                  Results
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   )
 }
 
